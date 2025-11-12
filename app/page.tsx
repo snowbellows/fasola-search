a32c2d6f771989d5e63d8ce25d7dfb3b771c85b0
Alex Halverson <alexhalverson@posteo.net>
2025-11-12 14:35:37 +1100

    feat(input): add note keys input

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,76 @@
-import Image from "next/image";
+'use client';
+
+import { ArrowLeftIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
+import { Item } from '@/components/ui/item';
 
 export default function Home() {
+  const [notes, setNotes] = useState({
+    first: '',
+    second: '',
+    third: '',
+    fourth: '',
+    fifth: '',
+    sixth: '',
+    seventh: '',
+    eighth: '',
+  });
+
+  const [currentNoteIndex, setCurrentNoteIndex] = useState(0);
+
+  function updateNote(note: string) {
+    const noteKeys = Object.keys(notes);
+
+    const key = noteKeys[currentNoteIndex];
+    setNotes((prevNotes) => ({
+      ...prevNotes,
+      [key]: note,
+    }));
+    if (currentNoteIndex < noteKeys.length - 1) {
+      setCurrentNoteIndex(currentNoteIndex + 1);
+    } else {
+      setCurrentNoteIndex(0);
+    }
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Fa So La Search
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+            Search for Sacred Harp songs by tune.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-row gap-4 text-base font-medium">
+          <Button variant="outline" size="icon" onClick={() => updateNote('f')}>
+            Fa
+          </Button>
+          <Button variant="outline" size="icon" onClick={() => updateNote('s')}>
+            So
+          </Button>
+          <Button variant="outline" size="icon" onClick={() => updateNote('l')}>
+            La
+          </Button>
+          <Button variant="outline" size="icon" onClick={() => updateNote('m')}>
+            Mi
+          </Button>
+          <Button>
+            <ArrowLeftIcon></ArrowLeftIcon>
+          </Button>
+        </div>
+        <div className="grid grid-cols-4 gap-2 text-base font-medium ">
+          {Object.entries(notes).map(([key, value], i) => (
+            <Item
+              key={key}
+              className={i === currentNoteIndex ? 'text-green-600' : ''}
+            >
+              {value || i + 1}
+            </Item>
+          ))}
         </div>
       </main>
     </div>
